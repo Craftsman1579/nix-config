63959918c87f7667dede08fd5031d3d783ca7afc
alex <baumgaertner.al@gmail.com>
2026-05-16 21:57:35 +0200

    update

--- hosts/my-macbook/configuration.nix
+++ hosts/my-macbook/configuration.nix
@@ -15,7 +15,20 @@
   home-manager.users.${primaryUser} = {
     home.packages = with pkgs; [
       graphite-cli
+      glab
     ];
+    programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      la = "ls -la";
+      ".." = "cd ..";
+      "nix-switch" = "sudo darwin-rebuild switch --flake ~/.config/nix";
+    };
+  };
 
   };
 }
