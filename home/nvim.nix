2c3effa5c711e8e7ca6619b4c20a2d85561ae9f8
alex <baumgaertner.al@gmail.com>
2026-05-17 22:12:09 +0200

    update

---
+++ home/nvim.nix
@@ -0,0 +1,19 @@
+{ ... }: {
+  programs.nixvim = {
+    enable = true;
+
+    opts = {
+      number = true;
+      relativenumber = true;
+      tabstop = 2;
+      shiftwidth = 2;
+      expandtab = true;
+    };
+
+    plugins = {
+      treesitter.enable = true;
+      telescope.enable = true;
+      lualine.enable = true;
+    };
+  };
+}
