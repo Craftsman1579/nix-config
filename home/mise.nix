4f8fafeb8292a817d968a2cb660d44b0c896c7db
alex <baumgaertner.al@gmail.com>
2026-05-17 21:44:27 +0200

    update

--- home/mise.nix
+++ home/mise.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ ... }:
 {
   programs.mise = {
     enable = true;
@@ -9,24 +9,18 @@
       verbose = false;
       auto_install = true;
     };
+
+    globalConfig = {
+      tools = {
+        node = "lts";
+        bun = "latest";
+        deno = "latest";
+        uv = "latest";
+        rust = "stable";
+      };
+      env = {
+        MISE_NODE_COREPACK = "true";
+      };
+    };
   };
-
-  # activation script to set up mise configuration
-  home.activation.setupMise = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    # use the virtual environment created by uv 
-    # ${pkgs.mise}/bin/mise settings set python.uv_venv_auto true
-
-    # enable corepack (pnpm, yarn, etc.)
-    ${pkgs.mise}/bin/mise set MISE_NODE_COREPACK=true
-
-    # disable warning about */.node-version files
-    ${pkgs.mise}/bin/mise settings add idiomatic_version_file_enable_tools "[]"
-
-    # set global tool versions (auto_install will handle installation)
-    ${pkgs.mise}/bin/mise use --global node@lts
-    ${pkgs.mise}/bin/mise use --global bun@latest
-    ${pkgs.mise}/bin/mise use --global deno@latest
-    ${pkgs.mise}/bin/mise use --global uv@latest
-    ${pkgs.mise}/bin/mise use --global rust@stable
-  '';
 }
