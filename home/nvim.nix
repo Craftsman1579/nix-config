af12a74451b7e7c52ba0196e30b542819c1515e2
alex <baumgaertner.al@gmail.com>
2026-05-18 12:50:40 +0200

    update

--- home/nvim.nix
+++ home/nvim.nix
@@ -16,7 +16,18 @@
       lualine.enable = true;
       web-devicons.enable = true;
       toggleterm.enable = true;
-
+      neo-tree = {
+        enable = true;
+        settings = {  
+          close_if_last_window = true;
+          filesystem = {
+            follow_current_file = {
+              enabled = true;
+              leave_dirs_open = true;
+            };
+          };
+        }
+      }
     };
   };
 }
