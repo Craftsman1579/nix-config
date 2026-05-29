fa3848f030d8e333dc392d875892108336c9fd33
Alexander Baumgärtner <alexander.baumgaertner@quanos.com>
2026-05-29 11:08:52 +0000

    update keymaps

--- home/terminal/nvim/lsp.nix
+++ home/terminal/nvim/lsp.nix
@@ -11,12 +11,48 @@
       servers = {
         ts_ls = {
           enable = true;
+          
+          # InlayHints: Typ-Informationen im Editor anzeigen
           settings.typescript.inlayHints = {
+            includeInlayParameterNameHints = "all";        # Parameternamen
+            includeInlayFunctionParameterTypeHints = true;  # Funktions-Param-Typen
+            includeInlayVariableTypeHints = true;           # Variablen-Typen
+            includeInlayPropertyDeclarationTypeHints = true; # Property-Typen
+            includeInlayFunctionReturnTypeHints = true;     # Return-Typen
+            includeInlayEnumMemberValueHints = true;        # Enum-Werte
+          };
+
+          # Code-Analyse & Diagnostik
+          settings.typescript = {
+            # Vorschläge & Auto-Fix
+            suggest = {
+              autoImports = true;
+              completeFunctionCalls = true;
+              completeJSDocs = true;
+            };
+            
+            # Null/Undefined-Checking
+            tsserver.maxTsServerMemory = 3072;  # RAM für TS-Server (MB)
+            
+            # Diagnostik-Filter
+            reportAssignmentType = true;
+            reportIncompatibleMethodOverride = true;
+            reportIncompatibleVariableOverride = true;
+          };
+
+          # JS/JSX-Dateien auch prüfen
+          settings.javascript.inlayHints = {
             includeInlayParameterNameHints = "all";
             includeInlayFunctionParameterTypeHints = true;
             includeInlayVariableTypeHints = true;
-            includeInlayPropertyDeclarationTypeHints = true;
           };
+        };
+
+        # ESLint als separater Server (nutzt deine eslint.config.mjs)
+        eslint = {
+          enable = true;
+          settings.run = "onSave";
+          settings.validate = [ "javascript" "javascriptreact" "typescript" "typescriptreact" ];
         };
 
         jsonls = {
@@ -59,7 +95,6 @@
 
         bashls.enable = true;
         dockerls.enable = true;
-        eslint.enable = true;
       };
     };
   };
