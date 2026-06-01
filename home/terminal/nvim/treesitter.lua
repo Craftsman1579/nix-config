4732ffb0eeaa23b9d6766f152855d5e17c3b2603
Alexander Baumgärtner <alexander.baumgaertner@quanos.com>
2026-06-01 09:32:52 +0000

    fix(nvim/treesitter): use nvim-treesitter 0.10+ API (setup({}))

--- home/terminal/nvim/treesitter.lua
+++ home/terminal/nvim/treesitter.lua
@@ -1,37 +1,9 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = {
-    'typescript','tsx','javascript',
-    'json','json5','yaml','toml',
-    'html','css','scss','graphql',
-    'lua','nix','bash','dockerfile',
-    'markdown','regex','vim','vimdoc',
-    'gitcommit','gitignore','diff','jsdoc'
-  },
-  highlight = { enable = true },
-  indent = { enable = true },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-        ['aa'] = '@parameter.outer',
-        ['ia'] = '@parameter.inner',
-      },
-    },
-    move = {
-      enable = true,
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-    },
-  },
-}
+-- nvim-treesitter 0.10+ setup
+-- Grammars are provided via nvim-treesitter.withPlugins (nix).
+-- Highlight/indent are enabled automatically; no extra config required.
+
+local ok, ts = pcall(require, 'nvim-treesitter')
+if ok then
+  -- Optionally point install_dir somewhere if needed; default is fine for runtime installs.
+  ts.setup({})
+end
